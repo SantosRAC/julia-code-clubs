585613c12d7873dfdbd0226aabf636986135aa68
Renato Augusto Corrêa dos Santos <renatoacsantos@gmail.com>
2021-10-24 23:11:56 -0300

    Added the hello world sentence

--- Intro_Julia_EstruturasDeDados.jl
+++ Intro_Julia_EstruturasDeDados.jl
@@ -1,8 +1,14 @@
+# Definindo variáveis em Julia
+
 #\alpha = 10
 alpha = 11
 
 #println(\alpha)
 println(alpha)
+
+#Imprimindo o que todo programador imprime quando aprende uma nova linguagem de programação
+println("Hello, World!")
+println('Hello, World!')
 
 # Definindo strings
 exampleString1 = "Fernanda"
