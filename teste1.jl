dada5641d8b1d9973bd07a31e1b2b9731722225e
Renato Augusto Corrêa dos Santos <renatoacsantos@gmail.com>
2021-10-24 23:00:59 -0300

    Updated jl script (added strings)

--- teste1.jl
+++ teste1.jl
@@ -1,6 +1,13 @@
-
 #\alpha = 10
 alpha = 11
 
 #println(\alpha)
 println(alpha)
+
+# Definindo strings
+exampleString1 = "Fernanda"
+exampleString2 = "Tamiris"
+println(exampleString1)
+println(exampleString2)
+
+# 
