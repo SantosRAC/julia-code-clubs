9ab48c734130ac874ac2356ee46cc9b6e6b63ce8
Python4MicrobioBrazil <python4microbiodatabrazil@gmail.com>
2021-11-24 22:45:04 -0300

    Added new script to store ERRORS; modified script with intro to variables

---
+++ BasicSteps/Codes/Erros.jl
@@ -0,0 +1,6 @@
+# Este código deve conter os principais erros observados ao longo da nossa trajetória
+
+# Erros de sintaxe::.
+# ERROR: syntax: "1" is not a valid function argument name
+# No seguinte caso, não podemos definir uma variável que comece com um caractere numérico
+1numero = 4
--- BasicSteps/Codes/Variaveis.jl
+++ BasicSteps/Codes/Variaveis.jl
@@ -1,23 +1,20 @@
 # Definindo variáveis em Julia
+# Usando variáveis
+# Operações numéricas e sintaxe
+# Tipos de variáveis
+# Conversão de variáveis
 
-#\alpha = 10
-alpha = 11
-nome = 'Qualquer coisa'
-
-#println(\alpha)
-println(alpha)
-
-#Imprimindo o que todo programador imprime quando aprende uma nova linguagem de programação
+# Imprimindo o que todo programador imprime quando aprende uma nova linguagem de programação
 # Strings, que veremos mais adiante, devem ser declaradas usando aspas duplas
 println("Hello, World!")
 
 # Este código não vai funcionar:
-#println('Hello, World!')
+println('Hello, World!')
 
-# Definindo strings
-exampleString1 = "Fernanda"
-exampleString2 = "Tamiris"
-println(exampleString1)
-println(exampleString2)
-
-# Concatenando strings
+# Regras para definição de variáveis
+# Elas DEVEM começar com letras
+numero = 4
+# Podem ter números, mas NÃO PODEM começar com número
+1numero = 4
+# Podem apresentar acentos, mas eu NÃO recomendo
+número = 4
