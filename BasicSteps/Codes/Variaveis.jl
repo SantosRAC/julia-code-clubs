5a843aaeb4c13083bf80df93f00307a21bfca707
Python4MicrobioBrazil <python4microbiodatabrazil@gmail.com>
2021-11-28 11:01:11 -0300

    Added additional examples of variable definition to script

--- BasicSteps/Codes/Variaveis.jl
+++ BasicSteps/Codes/Variaveis.jl
@@ -16,5 +16,19 @@ println('Hello, World!')
 numero = 4
 # Podem ter números, mas NÃO PODEM começar com número
 1numero = 4
+# O underscore pode ser usado para começar o nome da variável
+_1numero = 4
 # Podem apresentar acentos, mas eu NÃO recomendo
 número = 4
+
+# Não é possível atribuir variáveis com espaço
+numero 1 = 4
+# É possível, no entanto, usar "underscore" ("underline") para suprir a necessidade de um espaço
+numero_1 = 4
+# Outra forma de definir as variáveis, quando houver números
+numero1 = 4
+# Sugestões de definição de variáveis quando houver duas palavras compondo o nome
+nomeSujeito = "Renato"
+nomeDoSujeito = "Renato"
+nome_sujeito = "Renato"
+
