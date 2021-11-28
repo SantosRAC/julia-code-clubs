88b60396aceed36b50d77e08c11d57d4054a40b7
Python4MicrobioBrazil <python4microbiodatabrazil@gmail.com>
2021-11-28 10:55:02 -0300

    Added syntax error to file

--- BasicSteps/Codes/Erros.jl
+++ BasicSteps/Codes/Erros.jl
@@ -1,6 +1,9 @@
 # Este código deve conter os principais erros observados ao longo da nossa trajetória
 
-# Erros de sintaxe::.
+## Erros de sintaxe::.
 # ERROR: syntax: "1" is not a valid function argument name
-# No seguinte caso, não podemos definir uma variável que comece com um caractere numérico
+# No seguinte exemplo, não podemos definir uma variável que comece com um caractere numérico
 1numero = 4
+# ERROR: syntax: extra token "1" after end of expression
+# No seguinte exemplo, não podemos criar uma variável com espaço (\s)
+numero 1 = 4
