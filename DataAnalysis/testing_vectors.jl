6cbc69cae0fda4deed1841816219dff7677a0c16
Renato Augusto Corrêa dos Santos <renatoacsantos@gmail.com>
2022-06-03 17:35:39 -0300

    Delete testing_vectors.jl

--- DataAnalysis/testing_vectors.jl
+++
@@ -1,8 +0,0 @@
-using DataFrames, CSV
-
-cd(raw"/home/santosrac/Repositories/julia-code-clubs-ufabc/DataAnalysis")
-genome_info = DataFrame(CSV.File("genome_information.csv"))
-print(genome_info.Strain_Cultivar)
-
-#print(typeof(genome_info.Species))
-#print(typeof(convert(Array{String}, genome_info.Species)))
